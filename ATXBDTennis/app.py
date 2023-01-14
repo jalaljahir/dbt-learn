642f69383ed42f2234fd331a4a0327a0604a7682
jalaljahir <jalaljahir.hotmail.com>
2023-01-14 16:49:53 -0600

    back to .toml file

--- ATXBDTennis/app.py
+++ ATXBDTennis/app.py
@@ -48,9 +48,8 @@ def run_query(query):
     rows = rows.fetchall()
     return rows
 
-# sheet_url = st.secrets["public_gsheets_url"]
-sheet_url = str("https://docs.google.com/spreadsheets/d/1Cr9CW_l2PmTdpc6hZaqmjgS_hYBblTn3kWBcBIQazDk/edit#gid=0")
-sheet_url
+sheet_url = st.secrets["public_gsheets_url"]
+
 # print(sheet_url)
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 df = pd.DataFrame(rows)
