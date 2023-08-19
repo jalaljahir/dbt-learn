7c2b35bb91670d328c64c67c4b619ab5268bf85f
jalaljahir <jalaljahir.hotmail.com>
2023-08-19 10:55:48 -0500

    updated folder names

---
+++ 0006_ATXBDTennis/secret.py
@@ -0,0 +1,23 @@
+# SHEET_ID = '1lJ1nerbsKAv2ngce1rbGFtzva3iF64yFUf7A3G0ific'
+SHEET_ID = '1Cr9CW_l2PmTdpc6hZaqmjgS_hYBblTn3kWBcBIQazDk'
+#all schedule
+SHEET_NAME = 'Schedule'
+
+#schedule by team
+SHEET_NAME2 = 'Schedule_by_Team'
+
+#schedule by team SF/F
+SHEET_NAME3 = 'SF_F_Schedule_Team'
+
+#group score
+SHEET_NAME4= 'Score_Head_to_Head'
+SHEET_NAME5= 'Score_Final'
+
+# SF Score
+SHEET_NAME6= 'SF_Score_Master'
+SHEET_NAME7= 'SF_H_to_H'
+
+# F Score
+SHEET_NAME8= 'F_Score_Master'
+SHEET_NAME9= 'F_H_to_H'
+
